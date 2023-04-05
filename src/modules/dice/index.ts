a539a355a58e8682c9147f33573aebb7c537a135
ityuki <27209433+ityuki@users.noreply.github.com>
2023-04-05 21:08:48 +0900

    diceの成功不成功を追加

--- src/modules/dice/index.ts
+++ src/modules/dice/index.ts
@@ -17,23 +17,52 @@ export default class extends Module {
 	private async mentionHook(msg: Message) {
 		if (msg.text == null) return false;
 
-		const query = msg.text.match(/([0-9]+)[dD]([0-9]+)/);
+		const query = msg.text.match(/([0-9]+)[dD]([0-9]+)(?:\s*(\<|\<\=|\>|\>\=|\=|\=\=|\!\=)\s*([0-9]+))?/);
 
 		if (query == null) return false;
 
 		const times = parseInt(query[1], 10);
 		const dice = parseInt(query[2], 10);
+		const opt_test = query[3];
+		const opt_val = query[4];
+		if (!opt_val){ opt_val = parseInt(opt_val); }
 
 		if (times < 1 || times > 10) return false;
 		if (dice < 2 || dice > 1000) return false;
 
 		const results: number[] = [];
+		const opt_results : bool[] = [];
 
 		for (let i = 0; i < times; i++) {
-			results.push(Math.floor(Math.random() * dice) + 1);
+			let v = Math.floor(Math.random() * dice) + 1
+			results.push(v);
+			if (!opt_val){
+				switch(opt_test){
+					case "<": opt_results.push(v < opt_val); break;
+					case "<=": opt_results.push(v <= opt_val); break;
+					case ">": opt_results.push(v > opt_val); break;
+					case ">=": opt_results.push(v >= opt_val); break;
+					case "=": case "==": opt_results.push(v == opt_val); break;
+					case "!=": opt_results.push(v != opt_val); break;
+				}
+			}
 		}
-
-		msg.reply(serifs.dice.done(results.join(' ')));
+		
+		let opt_str = "";
+		if (!opt_val){
+			if (times == 1){
+				opt_str = opt_results[0] ? " 成功です！" : " 不成功です……";
+			}else{
+				let ok = 0;
+				for(let i=0;i<times;i++){
+					if (opt_results[i]){ ok++; }
+				}
+				let ng = times-ok;
+				opt_str = "  成功 " + String(ok) + " 件,  不成功 " + String(ng) " 件,  平均 " + String(Math.floor(100*ok/times)) + " % 位成功しました";
+			}
+		}
+		
+		msg.reply(serifs.dice.done(results.join(' ')) + opt_str);
 
 		return true;
 	}
